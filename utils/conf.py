37837cdd7c6fef3b81d93a84aefadad6c0d596dd
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-14 18:46:52 +0330

    Update conf.py

--- utils/conf.py
+++ utils/conf.py
@@ -92,8 +92,8 @@ def set_random_seed(seed: int) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     try:
-        torch.cuda.manual_seed_all(seed)
-        ##torch.cuda.manual_seed(seed)
+        ##torch.cuda.manual_seed_all(seed)
+        torch.cuda.manual_seed(seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
     except BaseException:
