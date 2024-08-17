af008b73a21570fbed3085d0b2be76ad3f75dde7
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-17 16:29:24 +0330

    Update agem.py

--- models/agem.py
+++ models/agem.py
@@ -8,16 +8,9 @@ import torch
 
 from models.gem import overwrite_grad, store_grad
 from models.utils.continual_model import ContinualModel
-from utils.args import *
+from utils.args import add_rehearsal_args, ArgumentParser
 from utils.buffer import Buffer
 
-
-def get_parser() -> ArgumentParser:
-    parser = ArgumentParser(description='Continual learning via A-GEM.')
-    add_management_args(parser)
-    add_experiment_args(parser)
-    add_rehearsal_args(parser)
-    return parser
 
 def project(gxy: torch.Tensor, ger: torch.Tensor) -> torch.Tensor:
     corr = torch.dot(gxy, ger) / torch.dot(ger, ger)
@@ -28,10 +21,16 @@ class AGem(ContinualModel):
     NAME = 'agem'
     COMPATIBILITY = ['class-il', 'domain-il', 'task-il']
 
+    @staticmethod
+    def get_parser() -> ArgumentParser:
+        parser = ArgumentParser(description='Continual learning via A-GEM.')
+        add_rehearsal_args(parser)
+        return parser
+
     def __init__(self, backbone, loss, args, transform):
         super(AGem, self).__init__(backbone, loss, args, transform)
 
-        self.buffer = Buffer(self.args.buffer_size, self.device)
+        self.buffer = Buffer(self.args.buffer_size)
         self.grad_dims = []
         for param in self.parameters():
             self.grad_dims.append(param.data.numel())
@@ -41,13 +40,13 @@ class AGem(ContinualModel):
     def end_task(self, dataset):
         samples_per_task = self.args.buffer_size // dataset.N_TASKS
         loader = dataset.train_loader
-        cur_y, cur_x, __ = next(iter(loader))[1:]
+        cur_y, cur_x = next(iter(loader))[1:]
         self.buffer.add_data(
             examples=cur_x.to(self.device),
             labels=cur_y.to(self.device)
         )
 
-    def observe(self, inputs, labels, not_aug_inputs, index_):
+    def observe(self, inputs, labels, not_aug_inputs, epoch=None):
 
         self.zero_grad()
         p = self.net.forward(inputs)
@@ -57,7 +56,7 @@ class AGem(ContinualModel):
         if not self.buffer.is_empty():
             store_grad(self.parameters, self.grad_xy, self.grad_dims)
 
-            buf_inputs, buf_labels = self.buffer.get_data(self.args.minibatch_size, transform=self.transform)
+            buf_inputs, buf_labels = self.buffer.get_data(self.args.minibatch_size, transform=self.transform, device=self.device)
             self.net.zero_grad()
             buf_outputs = self.net.forward(buf_inputs)
             penalty = self.loss(buf_outputs, buf_labels)
